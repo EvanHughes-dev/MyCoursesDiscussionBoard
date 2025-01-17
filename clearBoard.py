4261ddfcbc03c7089aeefa0e1a7e3e31e84ca2ac
Evan H <ejhingm@comcast.net>
2025-01-17 14:47:49 -0500

    Comment update

--- clearBoard.py
+++ clearBoard.py
@@ -28,19 +28,27 @@ for url_index in range(0, len(url_to_clear)):
     wait = WebDriverWait(driver, timeout=300)
     wait.until(EC.url_matches(url))
 
+    # the first time a link is executed, the user will be prompted to login to their account
+    # Once the user has logged in, the url will match the provided url, but none of the
+    # elements on the page will be loaded, causing driver.find_element to return an empty list
+    # to get around this, on the first loop reload the url. This forces the program to wait
+    # until the page has fully loaded
+
+    if url_index == 0:
+        driver.get(url)
     # find web links
     link = driver.find_elements(By.TAG_NAME, "a")
 
     # search each link for "discussions/threads"
     # add all links with that to a list to be visited
 
-    for i in link:
+    for thread in link:
         if (
-            i.get_attribute("href") != None
-            and i.get_attribute("href").find("discussions/threads") != -1
+            thread.get_attribute("href") != None
+            and thread.get_attribute("href").find("discussions/threads") != -1
         ):
-            links_to_visit.append(i.get_attribute("href"))
-
+            links_to_visit.append(thread.get_attribute("href"))
+print(len(links_to_visit))
 # visit all the discussion links
-for i in links_to_visit:
-    driver.get(i)
+for thread in links_to_visit:
+    driver.get(thread)
